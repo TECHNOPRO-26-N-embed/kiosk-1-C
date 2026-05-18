d60a19b153edd65928ba6c4c5e80bba29f2d82bf
Kawazoe-Shoto-TP <282411681+Kawazoe-Shoto-TP@users.noreply.github.com>
2026-05-18 15:05:10 +0900

    .cの追加

--- src/main.c
+++ src/main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include"start.c"
+int sceneNumber=0;
+
+int sceneChanger(int scene)
+{
+
+    sceneNumber=scene;
+
+    return scene;
+
+}
+
 int main(void) {
-printf("=== メニュー ===\n");
-printf("1: 機能A\n");
-printf("9: 終了\n");
+printf("========================= メニュー =========================\n");
+printf("\n\n\n\n\n");
+
+
+
+printf("1: 読み取り画面へ\t\t\t");
+printf("0: 終了");
+
+
+
+printf("\n");
+printf("=============================================================\n");
+
+printf("番号を入力\t:\t");
+scanf("%d",&sceneNumber);
+
+
+
+switch(sceneNumber)
+{
+    case 1:
+    action();
+    break;
+    case 9:
+    printf("終了します\n");
+    break;
+    default:
+    printf("無効な選択です\n");     
+
+
+}
+
 return 0;
+
 }
---
+++ src/start.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+void action()
+{
+    printf("start");
+
+
+
+
+
+
+    
+}
