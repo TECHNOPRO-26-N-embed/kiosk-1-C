ed7b3d0161570463a56c34c7194c1e8ac6657156
Homare-Tada-TP <282408972+Homare-Tada-TP@users.noreply.github.com>
2026-05-20 14:15:46 +0900

    a

--- src/product.c
+++ src/product.c
@@ -6,10 +6,20 @@
 // 商品マスタの初期化や商品登録・検索などを担当
 
 // 商品マスタ初期化
+<<<<<<< Updated upstream
 // master: 商品マスタ構造体へのポインタ
 void init_product_master(ProductMaster* master) {
     if (!master) return;
+=======
+ResultCode init_product_master(ProductMaster* master) {
+    if (!master) return RC_ERR;
+>>>>>>> Stashed changes
     master->product_count = 0;
     memset(master->products, 0, sizeof(master->products));
     memset(master->barcode_index, 0, sizeof(master->barcode_index));
+    return RC_OK;
+}
+
+ResultCode clear_product_master(ProductMaster* master) {
+    return init_product_master(master);
 }
